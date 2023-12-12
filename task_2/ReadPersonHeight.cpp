af569825aad70efd00afab5666454517a060e98f
DmitriyVx <113830810+DmitriyVx@users.noreply.github.com>
2023-12-12 20:26:52 +0300

    Add files via upload

---
+++ task_2/ReadPersonHeight.cpp
@@ -0,0 +1,7 @@
+#include"Header.h"
+unsigned short height;
+void ReadPersonHeight()
+{
+	cout << "¬ведите рост человека: ";
+	cin >> height;
+}
---
+++ task_2/ReadPersonName.cpp
@@ -0,0 +1,9 @@
+#include"Header.h"
+
+string ReadPersonName()
+{
+	cout << "¬ведите им€ человека: ";
+	string name;
+	cin >> name;
+	return name;
+}
