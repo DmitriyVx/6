6b2b6b0bbfddc48cceda726841de3129a8b41051
DmitriyVx <113830810+DmitriyVx@users.noreply.github.com>
2023-12-12 20:30:49 +0300

    Delete WritePersonData.cpp

--- WritePersonData.cpp
+++
@@ -1,8 +0,0 @@
-#include"Header.h"
-extern unsigned short height;
-extern double salary;
-void WritePersonData(const unsigned short* age, const string& name, const string& weight, const string& height)
-{
-	cout << " Имя " << name << " Вес " << weight << " Рост " << height;
-	cout << " Возраст " << *age << " Зарплата " << to_string(salary);
-}
